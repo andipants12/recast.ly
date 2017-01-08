c4cd33c517aab7a8df2dbf4a19dc869aa4f59fd9
Andrea Renee Oneto <andreareneeo@gmail.com>
2017-01-07 17:59:40 -0800

    with this commit I have passed all of the tests on the spec runner except for the tests regarding the youtube API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,38 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      video: exampleVideoData[0]
+    };
+  }
+
+  onTitleClick (video) {
+    this.setState({ video: video });
+  }
+
+	
+
+
+  render () {		
+    return (
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={ this.state.video } />
+        </div>
+        <div className="col-md-5">
+{/*can pass the methods through on the JSX references in an attribute*/}
+{/**/}
+          <VideoList handleClick={ this.onTitleClick.bind(this) } videos={ exampleVideoData }/>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 // TODO: Render the `App` component to the DOM
 
+// to get the watch working I ran this
+/*
+
+babel . --babel-watch --out-dir compiled --presets=es2015,react --ignore=node_modules,compiled --source-maps inline --watch
+
+*/
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
